026c2591a78e9d7e01c6d6ed2f67496babbccc53
atulsahu22032006 <archanabrijkishorsahu@gmail.com>
2026-08-21 06:20:09 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (50.44%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode temp = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+
+        while (temp != slow) {
+            temp = temp.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+}
