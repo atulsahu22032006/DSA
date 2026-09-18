4ae746cf2e2f713637fcad9217c4047717ec9baf
atulsahu22032006 <archanabrijkishorsahu@gmail.com>
2026-09-18 12:09:32 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (69.97%) - LeetSync

---
+++ TREE/94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -0,0 +1,16 @@
+
+class Solution {
+    public void dfs(TreeNode root, List<Integer> ans) {
+        if (root == null)
+            return;
+        dfs(root.left, ans);
+        ans.add(root.val);
+        dfs(root.right, ans);
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        dfs(root, ans);
+        return ans;
+    }
+}
